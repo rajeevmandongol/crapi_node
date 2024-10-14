13ef6903f46052dcdb2afb4e1c0eb0b915788084
rajeevmandongol <aaas_20999@yahoo.com>
2024-10-14 19:48:26 +0545

    Second Commit

--- netlify/functions/playerData.js
+++ netlify/functions/playerData.js
@@ -1,50 +1,62 @@
 // netlify/functions/playerData.js
 
-const axios = require('axios');
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 
 exports.handler = async (event, context) => {
-    const playerTag = event.queryStringParameters.playerTag;
+	const playerTag = event.queryStringParameters.playerTag;
 
-    // Check if playerTag is provided and valid
-    if (playerTag && /^[a-zA-Z0-9]+$/i.test(playerTag)) {
-        const formattedTag = encodeURIComponent(playerTag.toUpperCase());
-        const BASE_URL = process.env.BASE_URL;
-        const TOKEN = process.env.TOKEN; 
-        
-        const playerDataUrl = `${BASE_URL}players/%23${formattedTag}`;
-        const upcomingChestsUrl = `${BASE_URL}players/%23${formattedTag}/upcomingchests`;
-        const cardsUrl = `${BASE_URL}cards`;
+	// Check if playerTag is provided and valid
+	if (playerTag && /^[a-zA-Z0-9]+$/i.test(playerTag)) {
+		const formattedTag = encodeURIComponent(playerTag.toUpperCase());
+		const BASE_URL = process.env.BASE_URL;
+		const TOKEN = process.env.TOKEN;
 
-        try {
-            const playerData = await getDataFromServer(playerDataUrl, TOKEN);
-            const upcomingChests = await getDataFromServer(upcomingChestsUrl, TOKEN);
-            const cards = await getDataFromServer(cardsUrl, TOKEN);
+		const playerDataUrl = `${BASE_URL}players/%23${formattedTag}`;
+		const upcomingChestsUrl = `${BASE_URL}players/%23${formattedTag}/upcomingchests`;
+		const cardsUrl = `${BASE_URL}cards`;
 
-            return {
-                statusCode: 200,
-                body: JSON.stringify([playerData, upcomingChests, cards]),
-            };
-        } catch (error) {
-            return {
-                statusCode: 503,
-                body: JSON.stringify({ error: 'Service Unavailable' }),
-            };
-        }
-    } else {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ error: 'Invalid or missing playerTag' }),
-        };
-    }
+		try {
+			const playerData = await getDataFromServer(playerDataUrl, TOKEN);
+			const upcomingChests = await getDataFromServer(
+				upcomingChestsUrl,
+				TOKEN
+			);
+			const cards = await getDataFromServer(cardsUrl, TOKEN);
+
+			return {
+				statusCode: 200,
+				body: JSON.stringify({
+					success: true,
+					data: [playerData, upcomingChests, cards],
+				}),
+			};
+		} catch (error) {
+			return {
+				statusCode: 503,
+				body: JSON.stringify({
+					success: false,
+					message: "Service Unavailable",
+				}),
+			};
+		}
+	} else {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				success: false,
+				message: "Invalid or missing playerTag",
+			}),
+		};
+	}
 };
 
 async function getDataFromServer(url, token) {
-    const response = await axios.get(url, {
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-    });
-    return response.data;
+	const response = await axios.get(url, {
+		headers: {
+			Accept: "application/json",
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	return response.data;
 }
